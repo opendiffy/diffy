9bfb2f4a2ef993f450d7f29688d1028bf6fc7aff
Puneet Khanduri <puneet.khanduri@gmail.com>
2018-04-27 11:17:46 -0700

    renamed to diffy ai

--- src/main/scala/com/twitter/diffy/lifter/FieldMap.scala
+++
@@ -1,9 +0,0 @@
-package com.twitter.diffy.lifter
-
-import scala.collection.MapProxy
-
-case class FieldMap[T](override val self: Map[String, T]) extends MapProxy[String, T] {
-  override lazy val toString: String = {
-    self.toSeq.sortBy { case (k, v) => k }.toString
-  }
-}
