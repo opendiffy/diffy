e500a978d916cd6ab38d3f892b10e976eea36981
Puneet Khanduri <puneet.khanduri@gmail.com>
2023-06-18 22:45:13 +0530

    removed polling

--- frontend/src/features/endpoints/EndpointsView.tsx
+++ frontend/src/features/endpoints/EndpointsView.tsx
@@ -9,9 +9,9 @@ export default function EndpointsView() {
   const dispatch = useAppDispatch();
   const excludeNoise = useAppSelector((state) => state.selections.noiseCancellationIsOn);
   const selectedEndpoint = useAppSelector((state) => state.selections.endpointName) || 'undefined';
-  const [start, end] = useAppSelector((state) => state.selections.dateTimeRange).map(x=>x.getTime());
+  const [start, end] = useAppSelector((state) => state.selections.dateTimeRange);
   const noisyFields = useFetchNoiseQuery(selectedEndpoint).data || [];
-  const endpoints: Map<string, EndpointMeta> = useFetchEndpointsQuery({excludeNoise, start, end}, {pollingInterval: 10*1000}).data || new Map<string, EndpointMeta>();  
+  const endpoints: Map<string, EndpointMeta> = useFetchEndpointsQuery({excludeNoise, start, end}).data || new Map<string, EndpointMeta>();  
   const entries = Object.entries(endpoints);
   return (<List subheader={<ListSubheader>Endpoints</ListSubheader>}>
     {!entries.length?<ListItem><ListItemText>No endpoints.{excludeNoise && noisyFields.length?'(Some ignored)':''}</ListItemText></ListItem>:
